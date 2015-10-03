e65be28e02195de17b8c7c02b94ef0975d52935c
Summer <sbrochtrup@gmail.com>
2015-10-03 09:09:41 -0700

    Add dealsWithS method

--- src/main/java/Leetspeak.java
+++ src/main/java/Leetspeak.java
@@ -6,11 +6,10 @@ public class Leetspeak {
     public static void main(String[] args) {}
 
     public String leetspeak(String phrase) {
-    	// String[] words = phrase.split("\\s+");
-    	// String[] replacedWords = replaceLetters(words);
-    	// return replacedWords;
-    	String replacedPhrase = replaceLetters(phrase);
-    	return replacedPhrase;
+       	String replacedPhrase = replaceLetters(phrase);
+    	String newString = dealsWithS(replacedPhrase);
+    	String finalString = newString.substring(0, newString.length() -1);
+    	return finalString;
     }
 
     public String replaceLetters(String words) {
@@ -19,6 +18,23 @@ public class Leetspeak {
     		   		 replace("o", "0").
     		  	 	 replace("O", "0").
     	 	   		 replace("i", "1").
-   		       		 replace("I", "1");
+   		       		 replace("I", "1").
+   		       		 replace("s", "z").
+   		       		 replace("S", "Z");
+    }
+
+    public String dealsWithS(String words) {
+    	String[] wordsArray = words.split("\\s+");
+    	String resultingWords = "";
+    	for (String word : wordsArray) {
+    		if(word.substring(0,1).equals("z")) {
+    			resultingWords += ("s" + word.substring(1, word.length()) + " ");
+    		} else if (word.substring(0,1).equals("Z")) {
+    			resultingWords += ("S" + word.substring(1, word.length()) + " ");
+    		} else {
+    			resultingWords+=(word + " ");
+    		}
+    	}
+    	return resultingWords;
     }
 }
